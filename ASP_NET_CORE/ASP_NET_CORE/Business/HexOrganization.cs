8936d042253f01dc8be9e663d47fd675f94f57d7
BH713HE <BH713HE@IN1243676W1.ey.net>
2019-01-24 10:58:44 +0530

    test

---
+++ ASP_NET_CORE/ASP_NET_CORE/Business/HexOrganization.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+    using ASP_NET_CORE.Data;
+namespace ASP_NET_CORE.Business
+{
+    public class HexOrganization
+    {
+        public IEnumerable<String> GetEmployees()
+        {
+            return new HexOrganizationData().GetEmployees();
+        }
+    }
+}
---
+++ ASP_NET_CORE/ASP_NET_CORE/Data/HexOrganizationData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_NET_CORE.Data
+{
+    public class HexOrganizationData
+    {
+        public IEnumerable<String> GetEmployees()
+        {
+            return null;
+        }
+    }
+}
