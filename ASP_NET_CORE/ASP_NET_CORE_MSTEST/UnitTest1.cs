cfc856b56260e6f590841f8e6978f4016ef7f793
BH713HE <BH713HE@IN1243676W1.ey.net>
2019-01-25 15:33:11 +0530

    TEST

---
+++ ASP_NET_CORE/ASP_NET_CORE_MSTEST/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASP_NET_CORE_MSTEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ASP_NET_CORE/ASP_NET_CORE_TEST/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using ASP_NET_CORE.Data;
+namespace ASP_NET_CORE_TEST
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
